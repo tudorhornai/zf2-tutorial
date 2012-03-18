8c7bc74190a888250ebcecc2f705e67d2defd38c
Tudor Hornai <tudz_h@yahoo.com>
2012-03-18 11:31:30 +0200

    - add, edit, delete albums

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -3,11 +3,13 @@ namespace Album\Controller;
 
 use Zend\Mvc\Controller\ActionController,
     Zend\View\Model\ViewModel,
-    Album\Model\AlbumTable;
+    Album\Model\AlbumTable,
+    Album\Form\AlbumForm;;
     
 class AlbumController extends ActionController
 {
     protected $albumTable;
+    
     
     public function indexAction()
     {
@@ -16,16 +18,89 @@ class AlbumController extends ActionController
         ));
     }
     
+    
     public function addAction()
     {
+        $form = new AlbumForm();
+        $form->submit->setLabel('Add');
+        
+        $request = $this->getRequest();
+        if ($request->isPost()) 
+        {        
+            $formData = $request->post()->toArray();
+                        
+            if ($form->isValid($formData)) 
+            {            
+                $artist = $form->getValue('artist');
+                $title = $form->getValue('title');
+                $this->albumTable->addAlbum($artist, $title);
+                
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('default', array(
+                    'controller' => 'album',
+                    'action' => 'index',
+                ));
+            }
+        }
+        return array('form' => $form);
     }
+    
     
     public function editAction()
     {
+        $form = new AlbumForm();
+        $form->submit->setLabel('Edit');
+        $request = $this->getRequest();
+        if ($request->isPost()) 
+        {
+            $formData = $request->post()->toArray();
+            if ($form->isValid($formData)) 
+            {
+                $id = $form->getValue('id');
+                $artist = $form->getValue('artist');
+                $title = $form->getValue('title');
+                if ($this->albumTable->getAlbum($id)) 
+                {
+                    $this->albumTable->updateAlbum($id, $artist, $title);
+                }
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('default', array(
+                    'controller' => 'album',
+                    'action' => 'index',
+                ));
+            }
+        } 
+        else 
+        {
+            $id = $request->query()->get('id', 0);
+            if ($id > 0) 
+            {
+                $album = $this->albumTable->getAlbum($id);
+                $form->populate($album->getArrayCopy());
+            }
+        }
+        return array('form' => $form);
     }
     
     public function deleteAction()
     {
+        $request = $this->getRequest();
+        if ($request->isPost()) 
+        {
+            $del = $request->post()->get('del', 'No');
+            if ($del == 'Yes') 
+            {
+                $id = $request->post()->get('id');
+                $this->albumTable->deleteAlbum($id);
+            }
+            // Redirect to list of albums
+            return $this->redirect()->toRoute('default', array(
+            'controller' => 'album',
+            'action' => 'index',
+            ));
+        }
+        $id = $request->query()->get('id', 0);
+        return array('album' => $this->albumTable->getAlbum($id));
     }
     
     public function setAlbumTable(AlbumTable $albumTable)
